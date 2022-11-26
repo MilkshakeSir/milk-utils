9d11775c97d68710eab889f736ff4e8eea0a37d9
Milk <93629950+MilkdevNew@users.noreply.github.com>
2022-11-26 13:29:02 -0500

    Create DeviceFinder.lua

---
+++ Code/DeviceFinder.lua
@@ -0,0 +1,29 @@
+local DeviceFinder = {}
+local UserInputService = game:GetService("UserInputService")
+
+function DeviceFinder.getDevice()
+	local deviceInfo
+	local lastInput = UserInputService:GetLastInputType()
+	
+	
+	-- Computer check
+	if UserInputService.KeyboardEnabled and UserInputService.MouseEnabled then
+		if UserInputService.TouchEnabled then
+			deviceInfo = "NOW"
+		else
+			deviceInfo = "PC"
+		end
+	else
+		deviceInfo = "PHONE"
+	end
+	
+	
+	
+	if deviceInfo then
+		return deviceInfo
+	else
+		return "NIL"
+	end
+end
+
+return DeviceFinder
